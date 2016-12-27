0b652b9c84764a49ced129223ba4934e362601a0
zhaorui <zhaorui@sunyuki.com>
2016-12-27 18:05:12 +0800

    mvc train

--- src/MvcMovie/Controllers/MoviesController.cs
+++ src/MvcMovie/Controllers/MoviesController.cs
@@ -20,9 +20,34 @@ namespace MvcMovie.Controllers
         }
 
         // GET: Movies
-        public async Task<IActionResult> Index()
+        //public async Task<IActionResult> Index()
+        //{
+        //    return View(await _context.Movie.ToListAsync());
+        //}
+       
+        public async Task<IActionResult> Index(string movieGenre, string search)
         {
-            return View(await _context.Movie.ToListAsync());
+            var genreQuery = from m in _context.Movie
+                         orderby m.Genre
+                         select m.Genre;
+
+            var movies = from m in _context.Movie
+                         select m;
+
+            if (!String.IsNullOrEmpty(search))
+            {
+                movies = movies.Where(s => s.Title.Contains(search));
+            }
+
+            if (!String.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
+            }
+
+            var vm = new MovieGenreViewModel();
+            vm.genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+            vm.movies = await movies.ToListAsync();
+            return View(vm);
         }
 
         // GET: Movies/Details/5
@@ -53,7 +78,7 @@ namespace MvcMovie.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ID,Genre,Price,ReleaseDate,Title")] Movie movie)
+        public async Task<IActionResult> Create([Bind("ID,Genre,Price,ReleaseDate,Title,Rating")] Movie movie)
         {
             if (ModelState.IsValid)
             {
@@ -85,7 +110,7 @@ namespace MvcMovie.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ID,Genre,Price,ReleaseDate,Title")] Movie movie)
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Genre,Price,ReleaseDate,Title,Rating")] Movie movie)
         {
             if (id != movie.ID)
             {
--- src/MvcMovie/Models/Movie.cs
+++ src/MvcMovie/Models/Movie.cs
@@ -1,18 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MvcMovie.Models
 {
     public class Movie
     {
         public int ID { get; set; }
+
+        [StringLength(60,MinimumLength = 3)]
         public string Title { get; set; }
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode =true)]
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-z''-'\s]*$")]
+        [Required]
+        [StringLength(30)]
         public string Genre { get; set; }
+
+        [Range(1,100)]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-z''-'\s]*$")]
+        [StringLength(5)]
+        public string Rating { get; set; }
     }
 }
